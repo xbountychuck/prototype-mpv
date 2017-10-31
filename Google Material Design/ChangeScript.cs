d392b61bc0d1664448827c5f3cc9e2254d6a6f8a
xbountychuck <33233154+xbountychuck@users.noreply.github.com>
2017-10-30 22:20:09 -0500

    Add files via upload

---
+++ Google Material Design/ChangeScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class ChangeScript : MonoBehaviour
+{
+	public void changeToScene(int changeTheScene)
+	{
+		SceneManager.LoadScene (changeTheScene);
+		//	Debug.Log ("called");
+	}
+}
